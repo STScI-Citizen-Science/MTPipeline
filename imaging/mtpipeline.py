d3bff32e1fd274010ccba90fbc773546cef48608
Alex Viana <alexcostaviana@gmail.com>
2013-01-10 15:04:33 -0500

    Lots of changes to mtpipeline.py to address issue #18 as well as associated tests.

--- imaging/mtpipeline.py
+++ imaging/mtpipeline.py
@@ -22,6 +22,21 @@ from run_trim import run_trim
 # Functions (alphabetical)
 # ----------------------------------------------------------------------------
 
+def check_for_outputs(outputs):
+    '''
+    Returns True if all the outputs are present, else False.
+    '''
+    assert isinstance(outputs, str) or isinstance(outputs, list), \
+        'Expected str or list for outputs. Got ' + type(outputs)
+    if isinstance(outputs, str):
+        outputs = [outputs]
+    for item in outputs:
+        if not os.path.exists(item):
+            return False
+    return True
+
+# ----------------------------------------------------------------------------
+
 def make_output_file_dict(filename):
     '''
     Generate a dictionary with the list of expected inputs for each 
@@ -37,19 +52,27 @@ def make_output_file_dict(filename):
     output_file_dict['input_file'] = filename
     output_file_dict['cr_reject_output'] = []
     output_file_dict['drizzle_output'] = []
+    output_file_dict['png_output'] = []
     path, basename = os.path.split(filename)
     basename = basename.split('_')[0]
     
     # CR Rejection outputs.
-    for cr in ['','_cr']:
-        filename = os.path.join(path, basename + cr + '_c0m.fits')
+    for cr in ['_c0m.fits','_cr_c0m.fits']:
+        filename = os.path.join(path, basename + cr)
         output_file_dict['cr_reject_output'].append(filename)
             
     # Drizzled outputs.
-    for cr in ['','_cr']:
-        for drz in ['_wide', '_center']:
-            filename = os.path.join(path, basename + cr + '_c0m' + drz + '_single_sci.fits')
+    for cr in ['_c0m','_cr_c0m']:
+        for drz in ['_wide_single_sci.fits', '_center_single_sci.fits']:
+            filename = os.path.join(path, basename + cr + drz)
             output_file_dict['drizzle_output'].append(filename)
+
+    # PNG outputs.
+    for cr in ['_c0m','_cr_c0m']:
+        for drz in ['_wide', '_center']:
+            for png in ['_single_sci_log.png', '_single_sci_median.png']:
+                filename = os.path.join(path, basename + cr + drz + png)
+                output_file_dict['png_output'].append(filename)
     
     return output_file_dict
 
@@ -58,41 +81,52 @@ def make_output_file_dict(filename):
 # ----------------------------------------------------------------------------
 
 def run_mtpipeline(root_filename, output_path = None, cr_reject_switch=True, 
-        astrodrizzle_switch=True, trim_switch=True):
+        astrodrizzle_switch=True, png_switch=True, reproc_switch=False):
     '''
     This is the main controller for all the steps in the pipeline.
     '''
     # Generate the output drizzle names 
     filename = os.path.abspath(root_filename) 
     output_file_dict = make_output_file_dict(root_filename)
-    
+
     # Run CR reject.
-    if cr_reject_switch == True:
-        print 'Running cr_reject'
-        run_cosmics(root_filename, output_file_dict['cr_reject_output'][1])
-        print 'Done running cr_reject'
+    if cr_reject_switch:
+        output_check = check_for_outputs(output_file_dict['cr_reject_output'][1])
+        if reproc_switch == False and output_check == True:
+            print 'Not reprocessing cr_reject files.'
+        else:
+            print 'Running cr_reject'
+            run_cosmics(root_filename, output_file_dict['cr_reject_output'][1])
+            print 'Done running cr_reject'
     else:
         print 'Skipping cr_reject'
     
     # Run astrodrizzle.         
-    if astrodrizzle_switch == True:
-        print 'Running Astrodrizzle'
-        for filename in  output_file_dict['cr_reject_output']:
-            updatewcs.updatewcs(filename)
-            run_astrodrizzle(filename)
-        print 'Done running astrodrizzle'
+    if astrodrizzle_switch:
+        output_check = check_for_outputs(output_file_dict['drizzle_output'])
+        if reproc_switch == False and output_check == True:
+            print 'Not reprocessing astrodrizzle files.'
+        else:
+            print 'Running Astrodrizzle'
+            for filename in  output_file_dict['cr_reject_output']:
+                updatewcs.updatewcs(filename)
+                run_astrodrizzle(filename)
+            print 'Done running astrodrizzle'
     else:
         print 'Skipping astrodrizzle'
         
     # Run trim.
-    if trim_switch == True:
-        print 'Running scaling'
-        for filename in output_file_dict['drizzle_output']:
-            print filename
-            run_trim(filename, output_path, 'median')
-        print 'Done running scaling'
+    if png_switch:
+        output_check = check_for_outputs(output_file_dict['png_output'])
+        if reproc_switch == False and output_check == True:
+            print 'Not reprocessing png files.'
+        else:
+            print 'Running png'
+            for filename in output_file_dict['drizzle_output']:
+                run_trim(filename, output_path, 'median')
+            print 'Done running png'
     else:
-        print 'Skipping running scaling'
+        print 'Skipping running png'
         
 # ----------------------------------------------------------------------------
 # For command line execution.
@@ -127,12 +161,19 @@ def parse_args():
         dest = 'astrodrizzle',
         help = 'Toggle off the astrodrizzle step.')
     parser.add_argument(
-        '-no_trim',
+        '-no_png',
         required = False,
-        action='store_false',        
+        action = 'store_false',        
         default = True,
-        dest = 'trim',
-        help = 'Toggle off the scaling step.')
+        dest = 'png',
+        help = 'Toggle off the png step.')
+    parser.add_argument(
+        '-reproc',
+        required = False,
+        action = 'store_true',
+        default = False,
+        dest = 'reproc',
+        help = 'Reprocess all files, even if outputs already exist.')
     args = parser.parse_args()
     return args
 
@@ -147,4 +188,5 @@ if __name__ == '__main__':
             output_path =  args.output_path,
             cr_reject_switch = args.cr_reject,
             astrodrizzle_switch = args.astrodrizzle, 
-            trim_switch = args.trim)
+            png_switch = args.png,
+            reproc_switch = args.reproc)
--- imaging/mtpipeline_test.py
+++ imaging/mtpipeline_test.py
@@ -4,20 +4,16 @@
 Nosetest module for imaging/mtpipeline.py
 '''
 
+import os
+from mtpipeline import check_for_outputs
 from mtpipeline import make_output_file_dict
+
 
 def make_desired_result():
     '''
     Generated the dictionary of expected results.
     '''
-    desired_result = {}
-    desired_result['input_file'] = 'test_path/u2eu0101f_c0m.fits'
-    desired_result['cr_reject_output'] = ['test_path/u2eu0101f_c0m.fits', 
-        'test_path/u2eu0101f_cr_c0m.fits']
-    desired_result['drizzle_output'] = ['test_path/u2eu0101f_c0m_wide_single_sci.fits', 
-        'test_path/u2eu0101f_c0m_center_single_sci.fits', 
-        'test_path/u2eu0101f_cr_c0m_wide_single_sci.fits', 
-        'test_path/u2eu0101f_cr_c0m_center_single_sci.fits'] 
+    desired_result = make_output_file_dict('test_path/u2eu0101f_c0m.fits')
     return desired_result
 
 
@@ -46,3 +42,66 @@ def make_output_file_dict_test():
             zip(desired_result['drizzle_output'], test_result['drizzle_output']):
         assert desired_item == test_item, \
             '\nExpected: ' + str(desired_item) + '\nGot: ' + str(test_item)
+
+
+class TestCheckForOutputs(object):
+    '''
+    Tests for the check_for_outputs function. The setup and teardown 
+    methods create and remove the files the function will test for.
+    '''
+    def __init__(self):
+        '''
+        Creare the filename_dict and path attribues for use by all the 
+        other methods.
+        '''
+        self.filename_dict = make_desired_result()
+        self.path = os.path.split(self.filename_dict['input_file'])[0]
+
+    def make_file(self, filename):
+        '''
+        Create an empty dummy file.
+        '''
+        file_object = open(filename, 'w')
+        file_object.close()
+
+    def setup(self):
+        '''
+        Make the 'test_path' folder and dummy files for all the 
+        test files.
+        '''
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+        for value in self.filename_dict.itervalues():
+            if isinstance(value, str):
+                self.make_file(value)
+            elif isinstance(value, list):
+                for item in value:
+                    self.make_file(item)
+
+    def test_check_for_outputs_true(self):
+        '''
+        Test for files that exist.
+        '''
+        for value in self.filename_dict.itervalues():
+            query = check_for_outputs(value)
+            assert query == True, 'Exepected True for ' + str(value)
+
+    def test_check_for_outputs_false(self):
+        '''
+        Test for files that don't exist.
+        '''
+        for value in self.filename_dict.itervalues():
+            if isinstance(value, list):
+                value.append('does_not_exist.txt')
+            elif isinstance(value, str):
+                value = 'does_not_exist.txt'
+            query = check_for_outputs(value)
+            assert query == False, 'Exepected False for ' + str(value)
+
+    def teardown(self):
+        '''
+        Clean up all the test files.
+        '''
+        for filename in os.listdir(self.path):
+            os.remove(os.path.join(self.path, filename))
+        os.removedirs(self.path)
