4826a0ac8d375c101e523bf161e77d146134f12b
Alex Viana <alexcostaviana@gmail.com>
2013-01-24 21:17:54 -0500

    Refactor Database Declarative Classes
    ephem/build_master_table.py:
    	- Refactored the MasterImages class into database_interface.py. Changed the __init__ method into a load() method and updated the code accordingly. Tested on a live DB and works fine.

--- ephem/build_master_table.py
+++ ephem/build_master_table.py
@@ -6,45 +6,9 @@ import os
 import pyfits
 
 from database_interface import loadConnection
+from database_interface import MasterImages
 
 session, Base = loadConnection('mysql://root@localhost/mtpipeline')
-
-class MasterImages(Base):
-    '''
-    Class for interacting with the master_images MySQL table. 
-    Subclasses the Base class for the SQLAlchemy declarative base.
-    '''
-    __tablename__ = 'master_images'
-    __table_args__ = {'autoload':True}
-    def __init__(self, png_file, fits_file):
-        '''
-        The values for the minimum and maximum ra and dec are given in 
-        degrees to match the units of the RA_TARG and DEC_TARG 
-        keywords. calculated assuming the the HST pointing information 
-        is for pixels (420.0, 424.5). The width and hieght of the image
-        in pixels is taken from the NAXIS1 and NAXIS2 header keywords. 
-        The conversion used is (3,600 arcsec / 1 deg) * (20 pix / 1 arcsec)
-        = (72,000 pix / 1 deg). The pixel resolution is given in units 
-        of arcsec / pix.
-        '''
-        png_path, png_name = os.path.split(os.path.abspath(png_file))
-        fits_path, fits_name = os.path.split(os.path.abspath(fits_file))
-
-        self.project_id = pyfits.getval(fits_file, 'proposid')
-        self.name = png_name
-        self.fits_file = fits_name
-        self.object_name = pyfits.getval(fits_file, 'targname')
-        #self.set_id = 
-        #self.set_index =
-        self.width = pyfits.getval(fits_file, 'NAXIS1')
-        self.height = pyfits.getval(fits_file, 'NAXIS2')
-        self.minimum_ra = pyfits.getval(fits_file, 'RA_TARG') - (420.0 / 72000)
-        self.minimum_dec = pyfits.getval(fits_file, 'DEC_TARG') - (424.5 / 72000)
-        self.maximum_ra = pyfits.getval(fits_file, 'RA_TARG') + ((self.width - 420.0) / 72000) 
-        self.maximum_dec = pyfits.getval(fits_file, 'DEC_TARG') + ((self.height - 424.5) / 72000) 
-        self.pixel_resolution = 0.05 #arcsec / pix
-        self.description = pyfits.getval(fits_file, 'FILTNAM1')
-        self.file_location = png_path
 
 #----------------------------------------------------------------------------
 
@@ -102,10 +66,12 @@ if __name__ == '__main__':
         if args.rebuild == False:
             query = session.query(MasterImages.name).filter(MasterImages.name == png_name)
             if len(query.all()) == 0:
-                record = MasterImages(png_file, fits_file)
+                record = MasterImages()
+                record.load(png_file, fits_file)
                 session.add(record)
         elif args.rebuild == True:
-            record = MasterImages(png_file, fits_file)
+            record = MasterImages()
+            records.load(png_file, fits_file)
             session.add(record)           
 
     session.commit()
--- ephem/database_interface.py
+++ ephem/database_interface.py
@@ -5,6 +5,9 @@ Creates and returns a sqlalchemy session object using the declaarative
 base.
 '''
 
+import os
+import pyfits
+
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData
 from sqlalchemy.ext.declarative import declarative_base
@@ -34,6 +37,36 @@ class MasterImages(Base):
     '''
     __tablename__ = 'master_images'
     __table_args__ = {'autoload':True}
+
+    def load(self, png_file, fits_file):
+        '''
+        The values for the minimum and maximum ra and dec are given in 
+        degrees to match the units of the RA_TARG and DEC_TARG 
+        keywords. calculated assuming the the HST pointing information 
+        is for pixels (420.0, 424.5). The width and hieght of the image
+        in pixels is taken from the NAXIS1 and NAXIS2 header keywords. 
+        The conversion used is (3,600 arcsec / 1 deg) * (20 pix / 1 arcsec)
+        = (72,000 pix / 1 deg). The pixel resolution is given in units 
+        of arcsec / pix.
+        '''
+        png_path, png_name = os.path.split(os.path.abspath(png_file))
+        fits_path, fits_name = os.path.split(os.path.abspath(fits_file))
+
+        self.project_id = pyfits.getval(fits_file, 'proposid')
+        self.name = png_name
+        self.fits_file = fits_name
+        self.object_name = pyfits.getval(fits_file, 'targname')
+        #self.set_id = 
+        #self.set_index =
+        self.width = pyfits.getval(fits_file, 'NAXIS1')
+        self.height = pyfits.getval(fits_file, 'NAXIS2')
+        self.minimum_ra = pyfits.getval(fits_file, 'RA_TARG') - (420.0 / 72000)
+        self.minimum_dec = pyfits.getval(fits_file, 'DEC_TARG') - (424.5 / 72000)
+        self.maximum_ra = pyfits.getval(fits_file, 'RA_TARG') + ((self.width - 420.0) / 72000) 
+        self.maximum_dec = pyfits.getval(fits_file, 'DEC_TARG') + ((self.height - 424.5) / 72000) 
+        self.pixel_resolution = 0.05 #arcsec / pix
+        self.description = pyfits.getval(fits_file, 'FILTNAM1')
+        self.file_location = png_path
  
 class Finders(Base):
     '''
