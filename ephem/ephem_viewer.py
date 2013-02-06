0c447c3ee5db6dac2c8e741c84302317fdab79ba
Alex Viana <alexcostaviana@gmail.com>
2013-02-05 19:52:18 -0500

    Added a module for a command-line tool to overplot the ephemeride data.

---
+++ ephem/ephem_viewer.py
@@ -0,0 +1,101 @@
+#! usr/bin/env python
+
+'''
+Viewing tools for WFC3 images.
+'''
+
+import argparse
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+import os
+import pyfits
+
+from database_interface import loadConnection
+from PIL import Image
+
+#----------------------------------------------------------------------------
+# Connect the SQLAlchemy ORM declaritive base classes.
+#----------------------------------------------------------------------------
+
+session, Base = loadConnection('mysql+pymysql://root@localhost/mtpipeline')
+
+from database_interface import MasterImages
+from database_interface import MasterFinders
+
+#----------------------------------------------------------------------------
+# 
+#----------------------------------------------------------------------------
+
+class EphemPlot(object):
+    '''
+    Class for creating a overplot of the ephemerides.
+    '''
+    def __init__(self, filename):
+        '''
+        Take the png name and check the input.
+        '''
+        assert os.path.splitext(filename)[1] == '.png', 'Expected a .png file.'
+        self.filename = filename
+
+    def getEphem(self):
+        '''
+        Get the ephemerides information from the database.
+        '''
+        master_images_query = session.query(MasterImages.id).filter(\
+            MasterImages.name == os.path.basename(self.filename)).one()
+        master_finders_query = session.query(MasterFinders).filter(\
+            MasterFinders.master_images_id == master_images_query.id).count()
+        assert master_finders_query != 0, 'No record found in master_finders_query'
+        master_finders_query = session.query(MasterFinders).filter(\
+            MasterFinders.master_images_id == master_images_query.id).all()
+        self.master_finders_query = master_finders_query
+
+    def plot_image(self):
+        '''
+        Perform the actual plotting
+        '''
+        data = Image.open(self.filename)
+        fig = plt.figure()
+        ax1 = plt.subplot(111)
+        im = ax1.imshow(data)
+        ax1.set_xlim(ax1.get_xlim())
+        ax1.set_ylim(ax1.get_ylim())
+        for moon in self.master_finders_query:
+            ax1.plot(moon.ephem_x, moon.ephem_y, 'ro', markersize=10, markerfacecolor='none')
+            ax1.text(moon.ephem_x, moon.ephem_y, moon.object_name)
+        plt.draw()
+        plt.grid(True)
+        raw_input()
+
+#----------------------------------------------------------------------------
+
+def ephem_viewer_main(filename):
+    '''
+    The main controller.
+    '''
+    ep = EphemPlot(filename)
+    ep.getEphem()
+    ep.plot_image()
+    
+#----------------------------------------------------------------------------
+# For command line execution
+#----------------------------------------------------------------------------
+
+def parse_args():
+    '''
+    parse the command line arguemnts.
+    '''
+    parser = argparse.ArgumentParser(
+        description = 'Plots the ephemeride data from the database.')
+    parser.add_argument(
+        '-file',
+        required = True,
+        help = 'The file to display.')
+    args = parser.parse_args()
+    return args
+
+#----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    args = parse_args()
+    ephem_viewer_main(args.file)
