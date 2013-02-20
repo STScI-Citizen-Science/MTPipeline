2e8466bf7f08e56332e2c4e53ef3cb1759a3b4ce
Alex Viana <alexcostaviana@gmail.com>
2013-02-19 23:26:03 -0500

    Added a lot of new counters. For issue #31.

--- ephem/test_database.py
+++ ephem/test_database.py
@@ -6,43 +6,89 @@ Check to ensure that all the file counts are correct.
 import glob
 import os
 
-ARCHIVE = '/Users/viana/mtpipeline/archive'
-output_string = '| {:30} | {:10} |'
+#----------------------------------------------------------------------------
+# Load all the SQLAlchemy ORM bindings
+#----------------------------------------------------------------------------
 
-def counter(search_string):
+from database_interface import loadConnection
+from database_interface import Finders
+from database_interface import MasterFinders
+from database_interface import MasterImages
+from database_interface import SubImages
+from database_interface import SetsMasterImages
+
+session, Base = loadConnection('mysql+pymysql://root@localhost/mtpipeline')
+
+#----------------------------------------------------------------------------
+# Global Variables
+#----------------------------------------------------------------------------
+
+ARCHIVE = '/Users/viana/mtpipeline/archive'
+OUTPUT_STRING = '| {:35} | {:8} |'
+BORDER_STRING = '+ {:35} + {:8} +'
+
+#----------------------------------------------------------------------------
+# Functions
+#----------------------------------------------------------------------------
+
+def count_files(search_string):
     file_count = len(glob.glob(search_string))
     search_name = search_string.split('/')[-1]
-    print output_string.format(search_name, file_count)
+    print OUTPUT_STRING.format(search_name, file_count)
 
-print output_string.format(30 * '-', 10 * '-')
-print output_string.format('FITS Files', 'Count')
-print output_string.format(30 * '-', 10 * '-')
-proposal_count = len(glob.glob(os.path.join(ARCHIVE, '*/')))
-print output_string.format('Proposals', proposal_count)
-c0m_list = glob.glob(os.path.join(ARCHIVE, '*/*c0m.fits'))
-c0m_cr_list = [x for x in c0m_list if x.split('_')[2] == 'cr']
-print output_string.format('CR Rejected c0m.fits ', len(c0m_cr_list))
-c0m_count = len(c0m_list)
-print output_string.format('c0m.fits', c0m_count)
+def count_records():
+    pass
 
-search_list = ['*/*c1m.fits',
-    '*/*wide_single_sci.fits',
-    '*/*center_single_sci.fits',
-    '*/*single_sci.fits']
+if __name__ == '__main__':
+    print BORDER_STRING.format(35 * '-', 8 * '-')
+    print OUTPUT_STRING.format('FITS Files', 'Count')
+    print BORDER_STRING.format(35 * '-', 8 * '-')
+    proposal_count = len(glob.glob(os.path.join(ARCHIVE, '*/')))
+    print OUTPUT_STRING.format('Proposals', proposal_count)
+    c0m_list = glob.glob(os.path.join(ARCHIVE, '*/*c0m.fits'))
+    c0m_cr_list = [x for x in c0m_list if x.split('_')[2] == 'cr']
+    print OUTPUT_STRING.format('CR Rejected c0m.fits ', len(c0m_cr_list))
+    c0m_count = len(c0m_list)
+    print OUTPUT_STRING.format('c0m.fits', c0m_count)
+    search_list = ['*/*c1m.fits',
+        '*/*wide_single_sci.fits',
+        '*/*center_single_sci.fits',
+        '*/*single_sci.fits']
+    for search_string in search_list:
+        count_files(os.path.join(ARCHIVE, search_string))
+    print BORDER_STRING.format(35 * '-', 8 * '-')
+    print '\n'
 
-for search_string in search_list:
-    counter(os.path.join(ARCHIVE, search_string))
-print output_string.format(30 * '-', 10 * '-')
+    print BORDER_STRING.format(35 * '-', 8 * '-')
+    print OUTPUT_STRING.format('Master PNG Files', 'Count')
+    print BORDER_STRING.format(35 * '-', 8 * '-')
+    search_list = ['*/png/*linear.png',
+        '*/png/*wide*linear.png',
+        '*/png/*center*linear.png']
+    for search_string in search_list:
+        count_files(os.path.join(ARCHIVE, search_string))
+    print BORDER_STRING.format(35 * '-', 8 * '-')
+    print '\n'
 
-print '\n'
-print output_string.format(30 * '-', 10 * '-')
-print output_string.format('PNG Files', 'Count')
-print output_string.format(30 * '-', 10 * '-')
+    print BORDER_STRING.format(35 * '-', 8 * '-')
+    print OUTPUT_STRING.format('Subimage PNG Files', 'Count')
+    print BORDER_STRING.format(35 * '-', 8 * '-')
+    search_list = ['*/png/*linear_*.png',
+        '*/png/*wide*linear_*.png',
+        '*/png/*center*linear_*.png']
+    for search_string in search_list:
+        count_files(os.path.join(ARCHIVE, search_string))
+    print BORDER_STRING.format(35 * '-', 8 * '-')
+    print '\n'
 
+    table_class_list = [Finders, MasterFinders, MasterImages, SubImages, SetsMasterImages]
+    for table_class in table_class_list:
+        print BORDER_STRING.format(35 * '-', 8 * '-')
+        print OUTPUT_STRING.format(table_class.__tablename__, 'Records')
+        print BORDER_STRING.format(35 * '-', 8 * '-')
+        field_list = table_class.__table__.columns
+        for field in field_list:
+            print OUTPUT_STRING.format(field, session.query(table_class).filter(field != None).count())
+        print BORDER_STRING.format(35 * '-', 8 * '-')
+        print '\n'
 
-search_list = ['*/png/*linear.png',
-    '*/png/*linear_*.png']
-
-for search_string in search_list:
-    counter(os.path.join(ARCHIVE, search_string))
-print output_string.format(30 * '-', 10 * '-')
