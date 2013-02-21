3479f7d92d4fdf4dd36b3f2e0ec709246150d45a
Alex Viana <alexcostaviana@gmail.com>
2013-02-20 19:22:06 -0500

    Added code to update the visit, orbit, set_id, and set_index information. Also reworked the way I check assertsion, perform updates, and inserts. These changes will be pulled into the other modules as I keep working. Closes #34.

--- ephem/build_master_table.py
+++ ephem/build_master_table.py
@@ -1,9 +1,18 @@
 #! /usr/bin/env python
 
 import argparse
+import datetime
 import glob
 import os
 import pyfits
+
+from sqlalchemy.sql import func
+from sqlalchemy import desc
+from sqlalchemy import distinct
+
+#----------------------------------------------------------------------------
+# Load all the SQLAlchemy ORM bindings
+#----------------------------------------------------------------------------
 
 from database_interface import loadConnection
 from database_interface import MasterImages
@@ -11,6 +20,22 @@ from database_interface import MasterImages
 session, Base = loadConnection('mysql+pymysql://root@localhost/mtpipeline')
 
 #----------------------------------------------------------------------------
+
+
+def counter(count):
+    '''
+    Advance the count and print a status message every 100th item.
+    '''
+    check_type(count, int)
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    if count == 0:
+        print now + ': Starting processing'
+    count += 1
+    if count % 100 == 0:
+        print now + ': Completed ' + str(count)
+    check_type(count, int)
+    return count
+
 
 def get_fits_file(png_file):
     '''
@@ -31,6 +56,161 @@ def get_fits_file(png_file):
     assert os.path.splitext(fits_file)[1] == '.fits', 'unexpected output.'
     return fits_file
 
+
+def insert_record(record_dict):
+    '''
+    Insert the value into the database using SQLAlchemy.
+    '''
+    record = MasterImages()
+    for column in record.__table__.columns:
+        setattr(record, column, record_dict[column])
+    session.add(record)
+    session.commit()
+
+
+def make_record_dict(png_file, fits_file):
+    '''
+    Make a dictionary of all the data for a record.
+
+    The values for the minimum and maximum ra and dec are given in 
+    degrees to match the units of the RA_TARG and DEC_TARG 
+    keywords. calculated assuming the the HST pointing information 
+    is for pixels (420.0, 424.5). The width and hieght of the image
+    in pixels is taken from the NAXIS1 and NAXIS2 header keywords. 
+    The conversion used is (3,600 arcsec / 1 deg) * (20 pix / 1 arcsec)
+    = (72,000 pix / 1 deg). The pixel resolution is given in units 
+    of arcsec / pix.
+    '''
+    png_path, png_name = os.path.split(os.path.abspath(png_file))
+    fits_path, fits_name = os.path.split(os.path.abspath(fits_file))
+    record_dict = {}
+    record_dict['project_id'] = pyfits.getval(fits_file, 'proposid')
+    record_dict['name'] = png_name
+    record_dict['fits_file'] = fits_name
+    record_dict['object_name'] = pyfits.getval(fits_file, 'targname')
+    record_dict['width'] = pyfits.getval(fits_file, 'NAXIS1')
+    record_dict['height'] = pyfits.getval(fits_file, 'NAXIS2')
+    record_dict['minimum_ra'] = pyfits.getval(fits_file, 'RA_TARG') - \
+        (420.0 / 72000)
+    record_dict['minimum_dec'] = pyfits.getval(fits_file, 'DEC_TARG') - \
+        (424.5 / 72000)
+    record_dict['maximum_ra'] = pyfits.getval(fits_file, 'RA_TARG') + \
+        ((record_dict['width'] - 420.0) / 72000) 
+    record_dict['maximum_dec'] = pyfits.getval(fits_file, 'DEC_TARG') + \
+        ((record_dict['height'] - 424.5) / 72000) 
+    record_dict['pixel_resolution'] = 0.05 #arcsec / pix
+    record_dict['description'] = pyfits.getval(fits_file, 'FILTNAM1')
+    record_dict['file_location'] = png_path
+    linenum = pyfits.getval(fits_file, 'LINENUM')
+    record_dict['visit'] = linenum.split('.')[0]
+    record_dict['orbit'] = linenum.split('.')[1]
+    record_dict = make_set_info(record_dict)
+    return record_dict
+
+
+def check_type(instance, expected_type):
+    '''
+    A wrapper around my standard assert isinstance pattern.
+    '''
+    assert isinstance(instance, expected_type), \
+        'Expected ' + str(expected_type) + ' got ' +  \
+        str(type(record_dict)) + ' instead.'
+
+def make_set_info(record_dict):
+    '''
+    Use the existing dictionary information and the database to 
+    construct the set information and add it to the dictionary.
+    '''
+    check_type(record_dict, dict)
+
+    # If the set_index already exists keep the existing value. To 
+    # reprocess all set_id and set_index values they have to be set to 
+    # Null. Assumes a set_id exists if the set_index exists.
+    master_images_query = session.query(MasterImages).filter(\
+        MasterImages.name == record_dict['name']).filter(\
+        MasterImages.set_index != None)
+    assert master_images_query.count() <= 1, 'Got too many results.'
+    if master_images_query.count() == 1:
+        master_images_query = master_images_query.one()
+        record_dict['set_id'] = master_images_query.set_id
+        record_dict['set_index'] = master_images_query.set_index
+        return record_dict
+    
+    # If there isn't already a set_index, get all the records that 
+    # match the proposid, visit, and orbit, ignoring the records with 
+    # Null values.
+    set_matches = session.query(MasterImages).filter(\
+        MasterImages.project_id == record_dict['project_id']).filter(\
+        MasterImages.visit != None).filter(\
+        MasterImages.orbit != None).filter(\
+        MasterImages.visit == record_dict['visit']).filter(\
+        MasterImages.orbit == record_dict['orbit'])
+
+    # If all the set_id values are None.
+    if set_matches.filter(MasterImages.set_id != None).count() == 0:
+        max_set_id = session.query(func.max(MasterImages.set_id)).one()[0]
+        if max_set_id == None:
+            record_dict['set_id'] = 1
+            record_dict['set_index'] = 1
+        else:
+            record_dict['set_id'] = max_set_id + 1
+            record_dict['set_index'] = 1
+
+    # If the set_id already exists
+    else:
+        set_id = set_matches.filter(\
+            MasterImages.set_id != None).group_by(\
+            MasterImages.set_id).one()
+        record_dict['set_id'] = set_id.set_id
+        max_set_index = set_matches.filter(\
+            MasterImages.set_index != None).order_by(\
+            desc(MasterImages.set_index)).first()
+        record_dict['set_index'] = max_set_index.set_index + 1
+    return record_dict
+
+def update_record(record_dict, query):
+    '''
+    Update a record in the database using SQLAlchemy. See 
+    insert_record for details.
+    '''
+    check_type(record_dict, dict)
+    query.update(record_dict)
+    session.commit()
+
+#----------------------------------------------------------------------------
+# The main controller.
+#----------------------------------------------------------------------------
+
+def build_master_table_main(filelist, reproc, reproc_sets):
+    '''
+    The main controller.    
+    '''
+    if reproc == True and reproc_sets == False:
+        print 'REMINDER: You are reprocessing everything BUT the set information.'
+    if reproc_sets:
+        reproc = True
+        update_dict = {'set_id':None, 'set_index':None}
+        session.query(MasterImages).filter(\
+            MasterImages.set_index != None).update(update_dict)
+        session.commit()
+    file_list = glob.glob(filelist)
+    count = 0
+    for png_file in file_list:
+        fits_file = get_fits_file(png_file)
+        png_path, png_name = os.path. os.path.split(os.path.abspath(png_file))
+        master_images_query = session.query(MasterImages).filter(\
+            MasterImages.name == png_name)
+        assert master_images_query.count() in [0, 1], \
+            'Multiple matches for ' + png_name
+        if master_images_query.count() == 0:
+            record_dict = make_record_dict(png_file, fits_file)
+            insert_record(record_dict)
+        elif master_images_query.count() == 1 and reproc == True:
+            record_dict = make_record_dict(png_file, fits_file)
+            update_record(record_dict, master_images_query)
+        count = counter(count)
+    session.close()
+
 #----------------------------------------------------------------------------
 # For command line execution
 #----------------------------------------------------------------------------
@@ -46,12 +226,19 @@ def parse_args():
         required = True,
         help = 'Search string for files. Wildcards accepted.')
     parser.add_argument(
-        '-rebuild',
+        '-reproc',
         required = False,
         action='store_true',        
         default = False,
-        dest = 'rebuild',
-        help = 'Overwrite existing entries.')
+        dest = 'reproc',
+        help = 'Overwrite existing entries except for the set information.')
+    parser.add_argument(
+        '-reproc_sets',
+        required = False,
+        action='store_true',        
+        default = False,
+        dest = 'reproc_sets',
+        help = 'Overwrite existing set information.')
     args = parser.parse_args()
     return args
 
@@ -59,19 +246,4 @@ def parse_args():
 
 if __name__ == '__main__':
     args = parse_args()
-    file_list = glob.glob(args.filelist)
-    for png_file in file_list:
-        fits_file = get_fits_file(png_file)
-        png_path, png_name = os.path. os.path.split(os.path.abspath(png_file))
-        if args.rebuild == False:
-            query = session.query(MasterImages.name).filter(MasterImages.name == png_name)
-            if query.count() == 0:
-                record = MasterImages()
-                record.load(png_file, fits_file)
-                session.add(record)
-        elif args.rebuild == True:
-            record = MasterImages()
-            records.load(png_file, fits_file)
-            session.add(record)           
-        session.commit()
-    session.close()
+    build_master_table_main(args.filelist, args.reproc, args.reproc_sets)
