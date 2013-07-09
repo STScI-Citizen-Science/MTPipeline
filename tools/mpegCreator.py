7482d7ac5a818e17106d448a65ea2c4ed09d558f
prathamjoshi <pjoshi@bennington.edu>
2013-07-09 14:40:22 -0400

    made the suggested changes in mpegCreator.py and tested the script out

--- tools/mpegCreator.py
+++ tools/mpegCreator.py
@@ -14,7 +14,7 @@ import subprocess
 import os 
 
 
-PATH = "/astro/3/mutchler/mt/drizzled" #image path
+ROOTPATH = "/astro/3/mutchler/mt/drizzled" #image path
 
 def parse_args():
     '''
@@ -35,21 +35,21 @@ def buildMovies(movieType, path):
     """
     makes a subprocess call to the ffmpeg tool to create the movies.
     """
-    SOURCE=path.split('/')[6]
-    temp = os.path.join(PATH, 'temp')
+    source=path.split('/')[6]
+    temp = os.path.join(ROOTPATH, 'temp')
     listDir = os.listdir(path)
 
-    output = os.path.join(PATH, "movies", "temp", SOURCE + "All" + movieType + ".mp4") 
+    output = os.path.join(ROOTPATH, "movies", "temp", source + "All" + movieType + ".mp4") 
     subprocess.call(['ffmpeg', '-f', 'image2', '-r', '1',
                     '-pattern_type', 'glob', '-i','*'+ movieType + '*linear.png',
                     output])
     #for the comsmic ray rejected images   
-    output = os.path.join(PATH, "movies", "temp", SOURCE + "CR" + movieType + ".mp4") 
+    output = os.path.join(ROOTPATH, "movies", "temp", source + "CR" + movieType + ".mp4") 
     subprocess.call(['ffmpeg', '-f', 'image2', '-r', '1',
                     '-pattern_type', 'glob', '-i', '*cr*' + movieType + '*linear.png',
                     output])
     #for the non-cosmic ray rejected images
-    output = os.path.join(PATH, "movies", "temp", SOURCE + "nonCR" + movieType + ".mp4") 
+    output = os.path.join(ROOTPATH, "movies", "temp", source + "nonCR" + movieType + ".mp4") 
     #make a list of non-CR rejected images
     nonCRWideInput = [i for i in listDir if movieType in i and'linear' in i and 'cr' not in i]
     #copy all the non-CR rejected images to the temp directory
@@ -61,7 +61,7 @@ def buildMovies(movieType, path):
                 '-pattern_type', 'glob', '-i','*'+ movieType + '*linear.png',
                 output])
     subprocess.call('rm *.png', shell=True) #delete the temporary files 
-    os.chdir(path) #change back to our PATH
+    os.chdir(path) #change back to our ROOTPATH
 
 
 def runScript(path):
@@ -69,10 +69,9 @@ def runScript(path):
     run scripts for both center and wide images
     '''
     path = os.path.join(path,'png')
-    try:
-        os.chdir(path)
-    except:
-        return 
+    
+    os.chdir(path)
+
     buildMovies('wide', path)
     buildMovies('center', path)
     
@@ -82,18 +81,18 @@ def createMovie():
     all the subfolders and calls the runScript function accordingly.
     If no subfolder given calls runScript iteratively on all subfolders.
     """
-    path = PATH 
-    if SOURCE: #add sub-directory to path if given
-        path = PATH + '/' + SOURCE
+    path = ROOTPATH 
+    if source: #add sub-directory to path if given
+        path = ROOTPATH + '/' + source
         runScript(path)
     else: #else carry out operation for ALL sub-drectories
         for dirs in os.listdir(path):
             if dirs[0].isdigit():
-                path = PATH + '/' + dirs
+                path = ROOTPATH + '/' + dirs
                 runScript(path)
 
 
 if __name__ == '__main__':
     args = parse_args() 
-    SOURCE = args.source 
+    source = args.source 
     createMovie() 
