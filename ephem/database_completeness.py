ef11d5f82c048c477f2940c5dbf8739b3b5d7de7
Alex Viana <alexcostaviana@gmail.com>
2013-12-10 18:08:09 -0500

    Refactored database_completeness and added some formatting to the print statements.

--- ephem/database_completeness.py
+++ ephem/database_completeness.py
@@ -31,56 +31,72 @@ fits_count_dict = {'mars': defaultdict(int),
                    'neptune':defaultdict(int)}
 
 
-fits_query = session.query(MasterImages).all()
-for item in fits_query:
-    target = get_target_name(item)
-    fits_count_dict[target]['fits_count'] += 1
-    if item.name.split('_')[1] == 'cr':
-        fits_count_dict[target]['cr_count'] += 1
-    elif item.name.split('_')[1] == 'c0m':
-        fits_count_dict[target]['no_cr_count'] += 1
-    else:
-        print 'Derp!'
-        break
-print_dict(fits_count_dict)
+def get_fits_count():
+    """Get FITS Count from master_images table."""
+    fits_query = session.query(MasterImages).all()
+    for item in fits_query:
+        target = get_target_name(item)
+        fits_count_dict[target]['fits_count'] += 1
+        if item.name.split('_')[1] == 'cr':
+            fits_count_dict[target]['cr_count'] += 1
+        elif item.name.split('_')[1] == 'c0m':
+            fits_count_dict[target]['no_cr_count'] += 1
+        else:
+            print 'Derp!'
+            break
+    print '\nFITS Count from master_images table.'
+    print_dict(fits_count_dict)
 
-subimages_query = session.query(MasterImages, SubImages).\
-    join(SubImages, SubImages.master_images_id == MasterImages.id)\
-    .all()
-subimage_count_dict = {'mars': defaultdict(int), 
+
+def get_subimages_count():
+    """Get Subimage count from subimages table."""
+    subimages_query = session.query(MasterImages, SubImages).\
+        join(SubImages, SubImages.master_images_id == MasterImages.id)\
+        .all()
+    subimage_count_dict = {'mars': defaultdict(int), 
+                           'saturn':defaultdict(int), 
+                           'neptune':defaultdict(int)}
+    for record in subimages_query:
+        target = get_target_name(record.MasterImages)
+        subimage_count_dict[target]['total_records'] += 1
+    for key in subimage_count_dict:
+        subimage_count_dict[key]['expected_records'] = fits_count_dict[key]['fits_count'] * 13
+    print '\nSubimage count from subimages table.'
+    print_dict(subimage_count_dict)
+
+
+def get_moon_count():
+    """Get moon count from master_finders table."""
+    ephemeris_query = session.query(MasterFinders, MasterImages)\
+        .join(MasterImages, MasterImages.id == MasterFinders.master_images_id)\
+        .all()
+    #    .filter(MasterFinders.ephem_x >= 0)\
+    #    .filter(MasterFinders.ephem_y >= 0)\
+    #    .filter(MasterFinders.ephem_x <= 1725)\
+    #    .filter(MasterFinders.ephem_y <= 1300)\
+    #    .filter(MasterImages.drz_mode == 'wide')\
+
+    moons_per_planet_dict = {'neptune':14, 
+                             'pluto':5, 
+                             'jupiter':68, 
+                             'uranus':28, 
+                             'mars':3, 
+                             'saturn':62}
+
+    moon_count_dict = {'mars': defaultdict(int), 
                        'saturn':defaultdict(int), 
                        'neptune':defaultdict(int)}
-for record in subimages_query:
-    target = get_target_name(record.MasterImages)
-    subimage_count_dict[target]['total_records'] += 1
-for key in subimage_count_dict:
-    subimage_count_dict[key]['expected_records'] = fits_count_dict[key]['fits_count'] * 13
-print_dict(subimage_count_dict)
+    for item in ephemeris_query:
+        target = get_target_name(item.MasterImages)
+        if item.MasterFinders.ephem_x != None and item.MasterFinders.ephem_y != None:
+            moon_count_dict[target]['complete_records'] += 1
+        moon_count_dict[target]['total_records'] += 1
+    for key in moon_count_dict:
+        moon_count_dict[key]['expected_records'] = moons_per_planet_dict[key] * fits_count_dict[key]['fits_count']
+    print '\nMoon count from master_finders table.'
+    print_dict(moon_count_dict)
 
-ephemeris_query = session.query(MasterFinders, MasterImages)\
-    .join(MasterImages, MasterImages.id == MasterFinders.master_images_id)\
-    .all()
-#    .filter(MasterFinders.ephem_x >= 0)\
-#    .filter(MasterFinders.ephem_y >= 0)\
-#    .filter(MasterFinders.ephem_x <= 1725)\
-#    .filter(MasterFinders.ephem_y <= 1300)\
-#    .filter(MasterImages.drz_mode == 'wide')\
-
-moons_per_planet_dict = {'neptune':14, 
-                         'pluto':5, 
-                         'jupiter':68, 
-                         'uranus':28, 
-                         'mars':3, 
-                         'saturn':62}
-
-moon_count_dict = {'mars': defaultdict(int), 
-                   'saturn':defaultdict(int), 
-                   'neptune':defaultdict(int)}
-for item in ephemeris_query:
-    target = get_target_name(item.MasterImages)
-    if item.MasterFinders.ephem_x != None and item.MasterFinders.ephem_y != None:
-        moon_count_dict[target]['complete_records'] += 1
-    moon_count_dict[target]['total_records'] += 1
-for key in moon_count_dict:
-    moon_count_dict[key]['expected_records'] = moons_per_planet_dict[key] * fits_count_dict[key]['fits_count']
-print_dict(moon_count_dict)
+if __name__ == '__main__':
+    get_fits_count()
+    get_subimages_count()
+    get_moon_count()
