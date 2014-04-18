67da80c181f585063df25991abc4a1e978e86f71
Alex Viana <alexcostaviana@gmail.com>
2014-04-18 15:40:16 -0400

    Created a basic setup.py module and created mtpipeline as the package root.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='MTPipeline',
+      description='HST Moving Target Pipeline',
+      author='Space Telescope Science Institute',
+      url='https://github.com/STScI-Citizen-Science/MTPipeline',
+      packages=['mtpipeline'],
+     )
