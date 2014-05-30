f74f9b34aa508fea66d8df521a40b43dfa79404e
Wally Barbosa <walysson.vital@gmail.com>
2014-05-30 11:46:16 -0400

    Added docstring to check_filesystem_completeness.py and added content to template_settings.yaml. For ticket #80

--- scripts/checks/check_filesystem_completeness.py
+++ scripts/checks/check_filesystem_completeness.py
@@ -3,6 +3,11 @@
 from mtpipeline.get_settings import get_settings
 
 def check_filesystem_completeness_main():
+    """
+    
+    Printing the settings located in settings.yaml
+        
+    """
     print get_settings()
 
 if __name__ == "__main__":
