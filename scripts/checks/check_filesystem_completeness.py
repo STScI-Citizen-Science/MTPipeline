5160e9526668a1c6561963ba33f5c75ecce461b7
Wally Barbosa <walysson.vital@gmail.com>
2014-06-03 17:28:54 -0400

    Reimplemented the checking part algorithm. For ticket #80.

--- scripts/checks/check_filesystem_completeness.py
+++ scripts/checks/check_filesystem_completeness.py
@@ -9,34 +9,32 @@ import logging
 import os
 import time
 
-def check_file(filename):
-    """Check is the file exists, print an error."""
-    if not os.path.isfile(filename):
-        print 'Missing: {}'.format(filename)
-
-
-def check_file_completeness_main():
-    """The main functin for the check_file_completeness module."""
-    file_list = glob.glob(os.path.join(SETTINGS['wfpc2_output_path'], '*_*/*c0m.fits'))
-    file_list = [item for item in file_list if len(item) == 59]
+def check_filesystem_completeness_main():
+    """The main functin for the check_filesystem_completeness module."""
+    all_fits_file_list = glob.glob(os.path.join(SETTINGS['wfpc2_output_path'], '*_*/*.fits'))
+    c0m_file_list = [filename for filename in all_fits_file_list if filename.split('/')[-1].split('_')[-1] == 'c0m.fits']
+    fits_set = set(all_fits_file_list)
     
     logging.info('Checking in: {}'.format(SETTINGS['wfpc2_output_path']))
-    logging.info('Found {} root c0m.fits files'.format(len(file_list)))
+    logging.info('Found {} root c0m.fits files'.format(len(all_fits_file_list)))
     
     counter = 0
-    for c0m in file_list:
-        file_dict = make_output_file_dict(c0m)
-                    
-        # Check to see if the files exist
-        for value in file_dict.itervalues():
-            check_file(value)
-            counter += 1
-    
+    for filename in c0m_file_list:
+        file_dict = make_output_file_dict(filename)
+        for file in file_dict.itervalues():
+            if type(file) == 'str':
+                if file in fits_set:
+                    counter += 1
+            else:
+                for item in file:
+                    if item in fits_set:
+                        counter += 1
+
     logging.info('Checked {} files'.format(counter))
 
 if __name__ == '__main__':
     setup_logging()
     t1 = time.time()
-    check_file_completeness_main()
+    check_filesystem_completeness_main()
     t2 = time.time()
     logging.info('Ran in {} s'.format(int(float(t2 - t1))))
