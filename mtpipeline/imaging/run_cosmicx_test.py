5ec2ad6190d1a865bcf6fcf838e04e205189bb11
Kevin Hale <khale@itsd-summer04.stsci.edu>
2014-06-25 10:45:37 -0400

    Changed drive_cosmicx to run_cosmicx, to comply with naming conventions. Removed calls to cosmics.py methods in run_cosmics_test.py and imaging_pipeline.py, relaced them with calls to run_cosmicx.py. Renamed run_cosmics_test.py to run_cosmicx_test.py. Edited README.md to reflect use of lacosmicx.

---
+++ mtpipeline/imaging/run_cosmicx_test.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+'''
+Test module for the run_cosmics.py module.
+'''
+
+import os
+from run_cosmic import make_c1m_link  
+
+def setup():
+    query = os.path.islink('u2eu0101f_cr_c1m.fits')
+    if query == True:
+        os.remove('u2eu0101f_cr_c1m.fits')
+
+def teardown():
+    query = os.path.islink('u2eu0101f_cr_c1m.fits')
+    if query == True:
+        os.remove('u2eu0101f_cr_c1m.fits')
+
+def make_c1m_link_test():
+    '''
+    Test the make_c1m_link function. Uses a cleanup function to remove
+    the link.
+    '''
+    make_c1m_link('u2eu0101f_c0m.fits')
+    assert os.path.islink('u2eu0101f_cr_c1m.fits'), 'Link does not exist.'
+
