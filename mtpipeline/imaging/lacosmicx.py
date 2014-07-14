3f9f5caa4c6bf4aafdabec301e76c5c99c2a93e4
kevinfhale <khale@itsd-summer04.stsci.edu>
2014-07-14 12:24:35 -0400

    Removed some print statments from testing. For Ticket#113.

--- mtpipeline/imaging/lacosmicx.py
+++ mtpipeline/imaging/lacosmicx.py
@@ -9,13 +9,11 @@ def run(inmat,inmask=None,outmaskfile="",sigclip=3.0,objlim=5.0,sigfrac=0.1,satl
     # .... Check arguments, double NumPy matrices?
     test=numpy.zeros((2,2)) # create a NumPy matrix as a test object to check matin
     typetest= type(test) # get its type  
-    print 'hello'
     if type(inmat) != typetest:
         raise TypeError('In inmat, matrix argument is not *NumPy* array')
     if inmask is None:
         inmask=numpy.zeros(inmat.shape)
     if type(inmask) != typetest:
-        print 'hello5'
         raise TypeError('In inmask, matrix argument is not *NumPy* array')
     this_shape=inmat.shape
     nx=this_shape[1]
